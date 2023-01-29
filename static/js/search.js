baf0fc9b1a437008c06d3aa0237749ccfae8167a
NikitaNakahara <nikita.maer14@gmail.com>
2023-01-29 20:01:04 +0500

    created header of home page

---
+++ static/js/search.js
@@ -0,0 +1,38 @@
+let searchInput = document.querySelector('.header_search_input');
+let searchBtn = document.querySelector('.header_search_btn');
+
+searchState = "hidden";
+
+function openSearch() {
+    searchInput.style.borderLeft = "1px solid #4cbcb8";
+    searchInput.animate([
+        {width: "0"},
+        {width: "200px"}
+        ], {
+        duration: 200
+        });
+    searchInput.style.width = "200px";
+
+    searchState = "open";
+}
+
+function hiddenSearch() {
+    searchInput.style.borderLeft = "none";
+        searchInput.animate([
+            {width: "200px"},
+            {width: "0"}
+            ], {
+            duration: 200
+            });
+        searchInput.style.width = "0";
+
+        searchState = "hidden";
+}
+
+searchBtn.addEventListener('click', function(e) {
+    if (searchState == "hidden") {
+        openSearch();
+    } else {
+        hiddenSearch();
+    }
+});
